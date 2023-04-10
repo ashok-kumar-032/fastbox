d16c0d7c4328d371e1cf852359d0d21967e237b5
sukhvonder-jaat <monudhaka829@gmail.com>
2023-04-10 13:38:14 +0530

    hero

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Mynav from './components/Mynav';
+import { Hero } from './components/Hero';
 
 
 function App() {
   return (
     <div>
       <Mynav />
-
+      <Hero />
     </div>
   );
 }
