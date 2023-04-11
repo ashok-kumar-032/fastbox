5c45542b18a20bca23bb7a9e65b471f8b3d20975
sukhvonder-jaat <monudhaka829@gmail.com>
2023-04-11 09:36:29 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import { Container, Row, Col } from 'react-bootstrap'
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <Container>
+      <Row className=' vh-100 align-items-center'>
+        <Col lg={7}>
+          <p className='mb-0 clr_dark_green ff_Anybody fw_700 fs_90'>Largest and reliable courier service <br /> in your city </p>
+          <p className='mb-0 pb-5 pt-5 ff_Red-Hat-Text fw_400 fs_25 clr_grey'>Duis aute irure dolor in reprehenderit in voluptate cillum dolore <br /> eu fugiat nulla pariatur.</p>
+          <div className='d-inline-block input_box bg_light_green d-flex align-items-center justify-content-between'>
+            <input type="text" className='bg_light_green border-0 ff_Red-Hat-Text fw_400 fs_20 clr_grey' placeholder='Enter Tracking Number' />
+            <a className='checkbutton ff_poppins fw_500 fs_30 text-white text-center' href="#">Check</a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
---
+++ src/components/Imagesection.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+export const Imagesection = () => {
+    return (
+        <div>Imagesection</div>
+    )
+}
